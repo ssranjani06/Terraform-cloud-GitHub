2fb7a5750c19ad22312f66de492f987c3748c90f
ssranjani06 <99155402+ssranjani06@users.noreply.github.com>
2022-09-25 15:28:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,4 +16,6 @@ provider "aws" {
 resource "aws_instance" "Raminfra" {
 	ami = "ami-026b57f3c383c2eec" 
 	instance_type = "t2.micro"
+	vpc_id = "vpc-0284a09ac12fe41ac"
+	subnet_id = "subnet-07b603608da7e6acb"
 }
